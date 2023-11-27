15c69eade07d7a9b0ba50bd77b5e4d1f31b652be
unknown <alejandro.armas.programador@gmail.com>
2023-11-27 07:38:33 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pgl_desarrollo',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
